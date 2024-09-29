4d7c17fa0d672ebd34dbd13857057c980624fdc0
rahul <rs2570089@gmail.com>
2024-09-29 13:53:54 +0530

    Update

--- deploy.py
+++ deploy.py
@@ -6,18 +6,16 @@ def main():
     ec2_host = os.getenv('EC2_IP')
     ec2_user = os.getenv('EC2_USER')
     ec2_key = os.getenv('EC2_SECRET_KEY')
-
     private_key_path = "/tmp/ec2_key"
     with open(private_key_path, 'w') as f:
         f.write(ec2_key)
     os.chmod(private_key_path, 0o600)
 
-    key = paramiko.RSAKey.from_private_key_file(private_key_path)
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        client.connect(hostname=ec2_host, username=ec2_user, pkey=key)
+        client.connect(hostname=ec2_host, username=ec2_user, key_filename=private_key_path)
         stdin, stdout, stderr = client.exec_command("python app.py")
         print(stdout.read())
     except Exception as e:
